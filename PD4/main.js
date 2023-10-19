3e36aaf713fa453fa6627193e59a53948b4fbda3
Maksim Stecenko <maksim.stecenko@stud.vtmc.lt>
2023-10-19 12:12:28 +0300

    PD4 solution

---
+++ PD4/main.js
@@ -0,0 +1,16 @@
+//1.
+let petName;
+let petAge;
+//2.
+petName="Slavik";
+petAge=17;
+//3.
+console.log("Pet's name: " + petName);
+console.log("Pet's age: " + petAge);
+//4.
+petName="Nikas";
+petAge=13;
+//5.
+const buildingAddress="Nilo 12.";
+//6.
+console.log("Building Address: " + buildingAddress);
