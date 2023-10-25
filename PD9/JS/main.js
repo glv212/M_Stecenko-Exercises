da54a952799d015b12d5bf5f2eb20a8fea383dc4
M_Stecenko <maksim.stecenko@stud.vtmc.lt>
2023-10-26 02:57:19 +0300

    PD9 solutions

---
+++ PD9/JS/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let numberX = 10;
+let numberY = '20';
+console.log(numberX == numberY); // false
+console.log(numberX === numberY); // false
+console.log(numberX != numberY); // true
+console.log(numberX !== numberY); // true
+console.log(numberX > numberY); // false
+console.log(numberX >= numberY); // false
+console.log(numberX < numberY); // true
+console.log(numberX <= numberY); // true
+console.log((numberX != numberY) ? 'It is true' : 'It is false'); // It is true
