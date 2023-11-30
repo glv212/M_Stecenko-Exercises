46d75e1b648a30888eeaf5ff6bbd4cffb95eacd7
Arturas <arturasrizgelis3@gmail.com>
2023-11-30 14:14:35 +0200

    PD5 solutions

--- PD5/JS/main.js
+++ PD5/JS/main.js
@@ -1,46 +1,55 @@
-//1.
-let myNumber = 42;
-console.log("Numeris", muNumber);
-//2.
-let myBigInt = 465125671984526514;
-console.log("BigInt Numeris: ", myBigInt);
-//3.
-let mySting = "Mantukas Maltinukas";
-console.log("String: ", myString);
-//4.
-let userBoolean = "Boolean";
-console.log(userBoolean);
-//5.
-let myUnderFriend;
-console.log("Unfriend", myUnderFriend);
-//6.
-let myNull = null;
-console.log("Null: ", myNull);
-//7.
-let myObject = '1.50€';
-console.log("Maltinuku kaina: ", myObject);
-//8.
-let number = 12;
-let otherNumber = 45.89;
-let textOne = 'Beautiful JavaScript World';
-let textTwo = "Beautiful JavaScript World";
-let textThree = `Beautiful JavaScript World`;
-let isTrue = (true == false);
-let isEqual = (2 >= 0);
-let petName = undefined;
-let petAge = null;
-let human = {
-    humanName: undefined,
-    humanAge: null
-};
+"use strict";
 
-console.log("Typeof numeris:", typeof number);
-console.log("Typeof kitasNumeris:", typeof otherNumber);
-console.log("Typeof tekstas1:", typeof textOne);
-console.log("Typeof tekstas2:", typeof textTwo);
-console.log("Typeof tekstas3:", typeof textThree);
-console.log("Typeof tiesa:", typeof isTrue);
-console.log("Typeof lygus:", typeof isEqual);
-console.log("Typeof varas:", typeof petName);
-console.log("Typeof metai:", typeof petAge);
-console.log("Typeof human:", typeof human);
+let userNumber = "number";
+console.log(userNumber);
+
+let userBigint = "bigint";
+console.log(userBigint);
+
+let userString = "string";
+console.log(userString);
+
+let userBoolean = "boolean";
+console.log(userBoolean);
+
+let userUndefined = "undefined";
+console.log(userUndefined);
+
+let userNull = "null";
+console.log(userNull);
+
+let userObject = "object";
+console.log(userObject);
+
+let number = 12;
+console.log("number type is", typeof number);
+
+let otherNumber = 45.89;
+console.log("otherNumber type is", typeof otherNumber);
+
+let textOne = "Beautiful JavaScript World";
+console.log("textOne type is", typeof textOne);
+
+let textTwo = "Beautiful JavaScript World";
+console.log("textTwo type is", typeof textTwo);
+
+let textThree = `Beautiful JavaScript World`;
+console.log("textThree type is", typeof textThree);
+
+let isTrue = true == false;
+console.log("isTrue type is", typeof isTrue);
+
+let isEqual = 2 >= 0;
+console.log("isEqual type is", typeof isEqual);
+
+let petName = undefined;
+console.log("petName type is", typeof petName);
+
+let petAge = null;
+console.log("petAge type is", typeof petAge);
+
+let human = {
+  humanName: undefined,
+  humanAge: null,
+};
+console.log("human type is", typeof human);
